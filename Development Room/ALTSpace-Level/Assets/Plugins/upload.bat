7ef53002d697e12c9aebb46d9ac182357d7a9d96
matthewmungall96 <matthewmungall96@gmail.com>
2019-11-17 16:31:12 +0000

    Development Room V1.00
    Prototype version of the room

---
+++ Development Room/ALTSpace-Level/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\FiercePC\Documents\ALTSpace-Level\template.zip" https://account.altvr.com/api/space_templates/1334089889397866726.json
