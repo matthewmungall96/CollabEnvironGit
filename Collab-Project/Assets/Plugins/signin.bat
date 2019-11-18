19bc8ab4d079c8076ec9bf99c8f0bb40bb64d621
StephenHayward <stephenhaywardisme@gmail.com>
2019-11-18 19:36:40 +0000

    Add laptop work

---
+++ Collab-Project/Assets/Plugins/signin.bat
@@ -0,0 +1,3 @@
+echo off
+title Sign In to AltspaceVR
+curl -v -d "user[email]=warriorsofthebaguetteuws@gmail.com&user[password]=uwsWOTB90" https://account.altvr.com/users/sign_in.json -c cookie
---
+++ Collab-Project/Assets/Plugins/upload.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "space_template[zip]=@D:\UWS Coursework\CollabEnvironGit\Collab-Project\template.zip" https://account.altvr.com/api/space_templates/1338314714324664417.json
