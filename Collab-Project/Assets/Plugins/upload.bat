622644f3b459e264db38eaa7dc80e086be42ae7f
Stephen Hayward <stephen-hayward@hotmail.co.uk>
2019-11-19 21:38:36 +0000

    Update layout, Organise Assets

--- Collab-Project/Assets/Plugins/upload.bat
+++ Collab-Project/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@D:\UWS Coursework\CollabEnvironGit\Collab-Project\template.zip" https://account.altvr.com/api/space_templates/1338314714324664417.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\UWS Projects\UWS Collab Project\CollabEnvironGit\Collab-Project\template.zip" https://account.altvr.com/api/space_templates/1338314714324664417.json
