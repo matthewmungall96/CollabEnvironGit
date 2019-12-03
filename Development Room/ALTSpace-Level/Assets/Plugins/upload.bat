0b7a2db41d0a2b8aa59175573c0be982d80f0d6e
matthewmungall96 <matthewmungall96@gmail.com>
2019-12-03 20:52:44 +0000

    Updated

--- Development Room/ALTSpace-Level/Assets/Plugins/upload.bat
+++ Development Room/ALTSpace-Level/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@D:\GitHub Repositories\CollabEnvironGit\Development Room\ALTSpace-Level\template.zip" https://account.altvr.com/api/space_templates/1334089889397866726.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@C:\Users\FiercePC\Documents\GitHub\CollabEnvironGit\Development Room\ALTSpace-Level\template.zip" https://account.altvr.com/api/space_templates/1334089889397866726.json
